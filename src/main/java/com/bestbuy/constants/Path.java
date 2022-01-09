4edcb0bf586c9f3c9852d9fdb8578a21626ab3c7
Ir0nBlade <itsvimal@yahoo.com>
2022-01-09 01:19:58 +0000

    First Commit

---
+++ src/main/java/com/bestbuy/constants/Path.java
@@ -0,0 +1,15 @@
+package com.bestbuy.constants;
+
+/**
+ * @author Vimal Vithalpura
+ * @project BestBuy-Serenity-Homework-WK19
+ * @created 08/01/2022
+ */
+public class Path {
+
+    public static final String PRODUCTS = "/products";
+    public static final String STORES = "/stores";
+    public static final String CATEGORIES = "/categories";
+    public static final String SERVICES = "/services";
+
+}
---
+++ src/test/java/com/bestbuy/testbase/TestBaseForCategories.java
@@ -0,0 +1,9 @@
+package com.bestbuy.testbase;
+
+/**
+ * @author Vimal Vithalpura
+ * @project BestBuy-Serenity-Homework-WK19
+ * @created 08/01/2022
+ */
+public class TestBaseForCategories {
+}
